7e08885dcbaa8496828893b889ed0ac4a274039c
Jhonathan Moreira <arte.jhonathan@gmail.com>
2023-11-18 16:35:20 -0300

    Ajuste nome da empresa no perfil

--- vaga_emprego.php
+++ vaga_emprego.php
@@ -64,15 +64,6 @@
                 <div class="card-body">
                   <div class="row">
                     <div class="col-sm-3">
-                      <h6 class="mb-0">Empresa</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      <?php echo $dados['nome_empresa'];?>
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
                       <h6 class="mb-0">Email</h6>
                     </div>
                     <div class="col-sm-9 text-secondary">
