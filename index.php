874b72907f73d4eb4efdaeaa96393bb029b15ce4
Jhonathan Moreira <arte.jhonathan@gmail.com>
2023-11-19 10:30:40 -0300

    Ajuste imagem OpenGraph

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
+<!DOCTYPE html>
 <?php
   // error_reporting(E_ERROR | E_PARSE);
   include("classes/database.php");
   include("funcoes/funcoes.php");
   $banco = new BancoDeDados;
 ?>
-<!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
