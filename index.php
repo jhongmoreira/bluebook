088a63306623844392b25b4c3def0cc8b460a6a0
Jhonathan Moreira <arte.jhonathan@gmail.com>
2023-11-21 14:17:16 -0300

    Divisão do projeto para WP

--- index.php
+++ index.php
@@ -59,10 +59,10 @@
           <a class="nav-link" href="index.php?pg=3">WebTV</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
+      <!-- <form class="d-flex" role="search">
         <input class="form-control me-2 bg-dark text-white" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-warning" type="submit">Pesquisas</button>
-      </form>
+      </form> -->
     </div>
   </div>
 </nav>
--- inicio.php
+++ inicio.php
@@ -2,215 +2,101 @@
   include("classes/database.php");
   $banco = new BancoDeDados;
 ?>
-<div id="destaques" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-              <button type="button" data-bs-target="#destaques" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#destaques" data-bs-slide-to="1" aria-label="Slide 2"></button>
-              <!-- <button type="button" data-bs-target="#destaques" data-bs-slide-to="2" aria-label="Slide 3"></button> -->
-            </div>
 
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <div class="banner-1"></div>
-                <div class="container texto-container">
-                  <div class="carousel-caption text-start">
-                    <h1>Supermercado Central</h1>
-                    <p>Sua compra com o melhor preço da cidade.</p>
-                    <p><a class="btn btn-sm btn-primary" href="#">Acessar Feed</a></p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="carousel-item">
-                <div class="banner-2"></div>
-                <div class="container">
-                  <div class="carousel-caption">
-                    <h1>Farmácia São Sebastião</h1>
-                    <p>Descontos imperdíveis em medicamentos genéricos</p>
-                    <p><a class="btn btn-sm btn-primary" href="#">Acessar Feed</a></p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#destaques" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#destaques" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-            </button>
+      <div id="destaques" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#destaques" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#destaques" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <!-- <button type="button" data-bs-target="#destaques" data-bs-slide-to="2" aria-label="Slide 3"></button> -->
         </div>
 
-        <div class="container-fluid mt-4">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="banner-1"></div>
+            <div class="container texto-container">
+              <div class="carousel-caption text-start">
+                <h1>Supermercado Central</h1>
+                <p>Sua compra com o melhor preço da cidade.</p>
+                <p><a class="btn btn-sm btn-primary" href="#">Acessar Feed</a></p>
+              </div>
+            </div>
+          </div>
 
-        <div class="row">
-          <div class="col-md-12 mb-3">
-            <h2>Atualizações</h2>
-            <hr>
+          <div class="carousel-item">
+            <div class="banner-2"></div>
+            <div class="container">
+              <div class="carousel-caption">
+                <h1>Farmácia São Sebastião</h1>
+                <p>Descontos imperdíveis em medicamentos genéricos</p>
+                <p><a class="btn btn-sm btn-primary" href="#">Acessar Feed</a></p>
+              </div>
+            </div>
           </div>
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#destaques" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#destaques" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
 
 
-          <div class="row">
-            <?php 
-              $banco->query("SELECT * FROM categorias, noticias WHERE categoria = id_categoria ORDER BY id DESC LIMIT 4");
-              $total = $banco->linhas();
 
-              if ($total != 0)
-              {
-                  foreach ($banco->result() as $dados)
-                  {   
-              ?> 
-              
-                <div class="col-md-6">
-                  <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                    <div class="col p-4 d-flex flex-column">
-                      <strong class="d-inline-block mb-2 text-primary-emphasis"><?php echo $dados['nome_categoria'];?></strong>
-                      <h3 class="mb-0"><?php echo $dados['titulo'];?></h3>
-                      <div class="mb-1 text-body-secondary"><?php echo dataFormato($dados['data_noticia']);?></div>
-                      <p class="card-text mb-auto"><?php echo $dados['subtitulo'];?></p>
-                      <a href="index.php?pg=1&noticia=<?php echo $dados['identificador'];?>" class="icon-link gap-1 icon-link-hover stretched-link">
-                        Ler
-                      </a>
-                    </div>
-                    <div class="col-auto d-none d-lg-block">
-                      <img src="img/conteudo/<?php echo $dados['imagem_capa'];?>" class="thumb-noticia" alt="" srcset="">
-                    </div>
-                  </div>
-                </div>           
+      <section class="container-fluid mt-4">
 
-              <?php
+      <div class="row justify-content-center mb-5 mt-5">
+        <div class="col-md-8">
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Digite a empresa ou categoria" aria-label="Search">
+            <button class="btn btn-warning" type="submit">Pesquisar</button>
+          </form>
+        </div>
+      </div>
 
-                  }
-                }
+          <div class="container">
+            <div class="friend-list">
+              <div class="row">
+                <?php 
+                  $banco->query("SELECT * FROM  categorias, empresas WHERE categoria_empresa = id_categoria LIMIT 4");
+                  $total = $banco->linhas();
 
-                ?>
+                  if ($total != 0)
+                  {
+                      foreach ($banco->result() as $dados)
+                      {   
+                  ?> 
 
-              
-              <div class="col-md-12 mt-4 mb-5 d-flex justify-content-center">
-                <a href="index.php?pg=2" class="btn btn-sm btn-primary">
-                  Ver todas as Notícias
+                    <div class="col-md-4 col-sm-6">
+                      <div class="friend-card card">
+                          <img src="img/conteudo/background_profile.jpg" alt="profile-cover" class="img-responsive cover">
+                        <div class="card-info">
+                          <img src="img/conteudo/<?php echo $dados['logotipo_empresa'];?>" alt="user" class="profile-photo-lg">
+                          <div class="friend-info">
+                            <p class="float-end text-green">São Gotardo</p>
+                            <h5><a href="index.php?pg=4&cod=<?php echo $dados['id_empresa'];?>" class="profile-link"><?php echo $dados['nome_empresa'];?></a></h5>
+                            <p class="categoria-empresa"><?php echo $dados['nome_categoria'];?></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>      
+
+                  <?php
+                      }
+                    }
+                  ?>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12 mt-3 mb-5 d-flex justify-content-left">
+                <a href="index.php?pg=5" class="btn btn-sm btn-primary">
+                  Ver Empresas
                 </a>
               </div>
-          </div>
-
-          
-          <div class="row">
-            <div class="col-md-12 mb-3">
-              <h2>Empresas</h2>
-              <hr>
-            </div>
-          </div>
-
-          <div class="row">
-          <?php 
-              $banco->query("SELECT * FROM  categorias, empresas WHERE categoria_empresa = id_categoria LIMIT 4");
-              $total = $banco->linhas();
-
-              if ($total != 0)
-              {
-                  foreach ($banco->result() as $dados)
-                  {   
-              ?> 
-            <div class="col-md-3 mb-4">
-                <div class="card p-1">
-                    <img src="img/conteudo/<?php echo $dados['logotipo_empresa'];?>" class="card-img-top" alt="Logotipo da Exata Serviços">
-                    <span class="categoria badge text-bg-primary"><?php echo $dados['nome_categoria'];?></span>
-                    <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                    <small><i class="fa-brands fa-whatsapp"></i>
-                      <?php 
-                      if ($dados['whatsapp_empresa'] == ''){
-                        echo "Não informado";
-                      }else{
-                        echo formatar('(%s%s) %s%s%s%s-%s%s%s%s', $dados['whatsapp_empresa']);
-                      }
-                      ?>
-                    </small>
-                  </li>
-                  <li class="list-group-item">
-                    <small><i class="fa fa-phone"></i> 
-                      <?php 
-                      if ($dados['telefone_empresa'] == ''){
-                        echo "Não informado";
-                      }else{
-                        echo formatar('(%s%s) %s%s%s%s-%s%s%s%s', $dados['telefone_empresa']);
-                      }
-                      ?>
-                    </small>
-                  </li>
-                      <li class="list-group-item"><small><i class="fa fa-envelope"></i> <?php echo $dados['email_empresa'];?></small></li>
-                    </ul>
-                    <div class="card-body">
-                    <a href="index.php?pg=4&cod=<?php echo $dados['id_empresa'];?>" class="icon-link gap-1 icon-link-hover stretched-link text-center">
-                      Ver
-                    </a>                    
-                  </div>
-                </div>
-            </div>
-                  <?php
-                  }
-                }
-                  ?>
-
-            <div class="col-md-12 mt-3 mb-5 d-flex justify-content-center">
-              <a href="index.php?pg=5" class="btn btn-sm btn-primary">
-                Ver Empresas
-              </a>
-            </div>
-          </div>
-
-          <!-- Vagas de Emprego -->
-          <div class="row">
-            <div class="col-md-12 mb-3">
-              <h2>Vagas de Emprego</h2>
-              <hr>
-            </div>
-          </div>
-          <section class="row">
-          <?php 
-              $banco->query("SELECT * FROM  escolaridade, vagas, empresas WHERE cod_empresa = id_empresa AND cod_escolaridade = id_escolaridade ORDER BY id_vaga ASC LIMIT 6 ");
-              $total = $banco->linhas();
-
-              if ($total != 0)
-              {
-                  foreach ($banco->result() as $dados)
-                  {   
-              ?> 
-          <div class="col-md-2 mb-4">
-                <div class="card p-1">
-                    <img src="img/conteudo/<?php echo $dados['logotipo_empresa'];?>" class="card-img-top" alt="Logotipo da Exata Serviços">
-                    <ul class="list-group list-group-flush">
-                      <li class="list-group-item"><small><i class="fa-solid fa-briefcase"></i> <?php echo $dados['nome_vaga'];?></small></li>
-                      <li class="list-group-item"><small><i class="fa fa-solid fa-graduation-cap"></i> <?php echo $dados['tipo_escolaridade'];?></small></li>
-                    </ul>
-                    <?php 
-                        $status = ($dados['aberto'] == 1) ? "Disponível" : "Encerrado";
-                        
-                        $background_status = ($dados['aberto'] == 1) ? "text-bg-success" : "text-bg-danger";
-
-                      ?>
-                    <span class="categoria badge <?php echo $background_status; ?>">
-                      <?php echo $status; ?>
-                    </span>
-                    <div class="card-body">
-                    <a href="index.php?pg=7&vaga=<?php echo $dados['id_vaga'];?>" class="icon-link gap-1 icon-link-hover stretched-link text-center">
-                      Ver
-                    </a>                    
-                  </div>
-                </div>
-            </div>
-            <?php
-                  }
-                }
-                ?>
-
-            <div class="col-md-12 mt-5 mb-5 d-flex justify-content-center">
-              <a href="index.php?pg=6" class="btn btn-sm btn-primary">
-                Ver todas as Vagas
-              </a>
             </div>
 
-          </section>
-            </div>
+        </div>
+      </section>
