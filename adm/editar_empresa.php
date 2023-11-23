a63a1accad85106f533d49f0c432ec2ab1e7e397
Jhonathan Moreira <arte.jhonathan@gmail.com>
2023-11-23 19:30:20 -0300

    Ajuste formulário edicação

--- adm/editar_empresa.php
+++ adm/editar_empresa.php
@@ -19,111 +19,125 @@
 
         <form method="post" enctype="multipart/form-data">
 
-            <div class="form-group">
-                <label for="nomeEmpresa">Nome</label>
-                <input type="text" class="form-control" id="nomeEmpresa" name="nomeEmpresa" value="<?php echo $dados["nome_empresa"]; ?>">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="form-group">
+                    <label for="nomeEmpresa">Nome</label>
+                    <input type="text" class="form-control" id="nomeEmpresa" name="nomeEmpresa" value="<?php echo $dados["nome_empresa"]; ?>">
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="foto" class="form-label">Logotipo</label>
-                <input class="form-control" type="file" name="foto" id="foto">
-            </div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label for="idCategoria">Categoria:</label>
+                    <select name="idCategoria" id="idCategoria" class="form-control">
+                        <?php
+                            $bancoCategoria = new BancoDeDados;
+                            $bancoCategoria->query("SELECT * FROM categorias");
+                            $totalCategoria = $bancoCategoria->linhas();
 
-            <div class="form-group">
-                <label for="idCategoria">Categoria:</label>
-                <select name="idCategoria" id="idCategoria" class="form-control">
-                    <?php
-                        $bancoCategoria = new BancoDeDados;
-                        $bancoCategoria->query("SELECT * FROM categorias");
-                        $totalCategoria = $bancoCategoria->linhas();
-
-                        if ($totalCategoria != 0){
-                            foreach ($bancoCategoria->result() as $dadosCategoria)
-                            {     
-                    ?> 
-                    <option value="<?php echo $dadosCategoria['id_categoria'];?>"><?php echo $dadosCategoria['nome_categoria'];?></option>
-                    <?php
+                            if ($totalCategoria != 0){
+                                foreach ($bancoCategoria->result() as $dadosCategoria)
+                                { 
+                                $selected = ($dados["categoria_empresa"] == $dadosCategoria["id_categoria"]) ? "selected" : "";  
+                        ?> 
+                        <option <?php echo  $selected; ?> value="<?php echo $dadosCategoria['id_categoria'];?>"><?php echo $dadosCategoria['nome_categoria'];?></option>
+                        <?php
+                                }
                             }
-                        }
-                    ?>
-                </select>
+                        ?>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-2">
+                <div class="form-group">
+                    <label for="telefoneEmpresa">Telefone</label>
+                    <input type="text" class="form-control" id="telefoneEmpresa" name="telefoneEmpresa" value="<?php echo $dados["telefone_empresa"]; ?>">
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="telefoneEmpresa">Telefone</label>
-                <input type="text" class="form-control" id="telefoneEmpresa" name="telefoneEmpresa" value="<?php echo $dados["telefone_empresa"]; ?>">
+            <div class="col-md-2">
+                <div class="form-group">
+                    <label for="whatsappEmpresa">Whatsapp</label>
+                    <input type="text" class="form-control" id="whatsappEmpresa" name="whatsappEmpresa" value="<?php echo $dados["whatsapp_empresa"]; ?>">
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="whatsappEmpresa">Whatsapp</label>
-                <input type="text" class="form-control" id="whatsappEmpresa" name="whatsappEmpresa" value="<?php echo $dados["whatsapp_empresa"]; ?>">
+            <div class="col-md-2">
+                <div class="form-group">
+                    <label for="siteEmpresa">Site</label>
+                    <input type="text" class="form-control" id="siteEmpresa" name="siteEmpresa" value="<?php echo $dados["site_empresa"]; ?>">
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="siteEmpresa">Site</label>
-                <input type="text" class="form-control" id="siteEmpresa" name="siteEmpresa" value="<?php echo $dados["site_empresa"]; ?>">
+            <div class="col-md-2">                
+                <div class="form-group">
+                    <label for="instagramEmpresa">Instagram</label>
+                    <input type="text" class="form-control" id="instagramEmpresa" name="instagramEmpresa" value="<?php echo $dados["instagram_empresa"]; ?>">
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="instagramEmpresa">Instagram</label>
-                <input type="text" class="form-control" id="instagramEmpresa" name="instagramEmpresa" value="<?php echo $dados["instagram_empresa"]; ?>">
+            <div class="col-md-2">
+                <div class="form-group">
+                    <label for="facebookEmpresa">Facebook</label>
+                    <input type="text" class="form-control" id="facebookEmpresa" name="facebookEmpresa" value="<?php echo $dados["facebook_empresa"]; ?>">
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="facebookEmpresa">Facebook</label>
-                <input type="text" class="form-control" id="facebookEmpresa" name="facebookEmpresa" value="<?php echo $dados["facebook_empresa"]; ?>">
+            <div class="col-md-2">
+                <div class="form-group">
+                    <label for="emailEmpresa">E-mail</label>
+                    <input type="text" class="form-control" id="emailEmpresa" name="emailEmpresa" value="<?php echo $dados["email_empresa"]; ?>">
+                </div>
+            </div>
+        </div>      
+        
+        <div class="row">
+            <div class="col-md-5">
+                <div class="form-group">
+                    <label for="ruaEmpresa">Rua</label>
+                    <input type="text" class="form-control" id="ruaEmpresa" name="ruaEmpresa" value="<?php echo $dados["rua_empresa"]; ?>">
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="emailEmpresa">E-mail</label>
-                <input type="text" class="form-control" id="emailEmpresa" name="emailEmpresa" value="<?php echo $dados["email_empresa"]; ?>">
+            <div class="col-md-1">
+                <div class="form-group">
+                    <label for="numeroEmpresa">Número</label>
+                    <input type="text" class="form-control" id="numeroEmpresa" name="numeroEmpresa" value="<?php echo $dados["numero_empresa"]; ?>">
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="ruaEmpresa">Rua</label>
-                <input type="text" class="form-control" id="ruaEmpresa" name="ruaEmpresa" value="<?php echo $dados["rua_empresa"]; ?>">
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="bairroEmpresa">Bairro</label>
+                    <input type="text" class="form-control" id="bairroEmpresa" name="bairroEmpresa" value="<?php echo $dados["bairro_empresa"]; ?>">
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="numeroEmpresa">Número</label>
-                <input type="text" class="form-control" id="numeroEmpresa" name="numeroEmpresa" value="<?php echo $dados["numero_empresa"]; ?>">
+            <div class="col-md-3">                
+                <div class="form-group">
+                    <label for="cidadeEmpresa">Cidade</label>
+                    <input type="text" class="form-control" id="cidadeEmpresa" name="cidadeEmpresa" value="<?php echo $dados["cidade_empresa"]; ?>">
+                </div>
             </div>
+        </div>
 
-            <div class="form-group">
-                <label for="bairroEmpresa">Bairro</label>
-                <input type="text" class="form-control" id="bairroEmpresa" name="bairroEmpresa" value="<?php echo $dados["bairro_empresa"]; ?>">
+        <div class="row mt-3 mb-3">
+            <div class="col-md-3">
+                <button type="submit" class="btn btn-primary">Enviar</button>
             </div>
-
-            <div class="form-group">
-                <label for="cidadeEmpresa">Cidade</label>
-                <input type="text" class="form-control" id="cidadeEmpresa" name="cidadeEmpresa" value="<?php echo $dados["cidade_empresa"]; ?>">
-            </div>
-    
-            <button type="submit" class="btn btn-primary mt-2 mb-5">Enviar</button>
-
+        </div>  
         </form>
 
+        <div class="row">
+            <div class="col-md-12">
 <?php
 if ($_SERVER["REQUEST_METHOD"] == 'POST'){
 
-    if ($_FILES["foto"]["error"]==4){
-        $erro = 1;
-    }else{
-        $erro = 0;
-        $diretorio = "../img/conteudo/";
-        $nome_arquivo = uniqid() . "_" . basename($_FILES["foto"]["name"]);
-        $caminho_arquivo = $diretorio . $nome_arquivo;
-
-        if (move_uploaded_file($_FILES["foto"]["tmp_name"], $caminho_arquivo)){
-            $fotoCapa = $nome_arquivo;
-            }else{
-                echo "Erro ao fazer upload";
-            }
-    }  
-
-
   $nome = addslashes($_POST["nomeEmpresa"]);
-  $logotipo = @$fotoCapa;
   $categoria = addslashes($_POST["idCategoria"]);  
   $telefone = addslashes($_POST["telefoneEmpresa"]);
   $whatsapp = addslashes($_POST["whatsappEmpresa"]);
@@ -136,34 +150,20 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST'){
   $bairro = addslashes($_POST["bairroEmpresa"]);
   $cidade = addslashes($_POST["cidadeEmpresa"]);
 
-    if ($erro==1)
-    {
-        $banco->query("UPDATE empresas SET nome_empresa = '$nome', categoria_empresa = $categoria, telefone_empresa = '$telefone', whatsapp_empresa = '$whatsapp', site_empresa = '$site', instagram_empresa = '$instagram', facebook_empresa = '$facebook', email_empresa = '$email', rua_empresa = '$rua', numero_empresa = $numero, bairro_empresa = '$bairro', cidade_empresa = '$cidade' WHERE id_empresa = $idEmpresa");
-    }
+    $banco->query("UPDATE empresas SET nome_empresa = '$nome', categoria_empresa = $categoria, telefone_empresa = '$telefone', whatsapp_empresa = '$whatsapp', site_empresa = '$site', instagram_empresa = '$instagram', facebook_empresa = '$facebook', email_empresa = '$email', rua_empresa = '$rua', numero_empresa = $numero, bairro_empresa = '$bairro', cidade_empresa = '$cidade' WHERE id_empresa = $idEmpresa");
     
-    if ($erro==0){
-        // $banco->query("UPDATE empresas SET nome_empresa = '$nome', logotipo_empresa = '$logotipo', categoria_empresa = $categoria, telefone_empresa = '$telefone', whatsapp_empresa = '$whatsapp', site_empresa = '$site', instagram_empresa = '$instagram', facebook_empresa = '$facebook', email_empresa = '$email', rua_empresa = '$rua', numero_empresa = $numero, bairro_empresa = '$bairro', cidade_empresa = '$cidade' WHERE id_empresa = $idEmpresa");
-        echo "tá foda";
-        echo "tá foda";
-        echo "tá foda";
-        echo "tá foda";
-        echo "tá foda";
-        echo "tá foda";
-        echo "tá foda";
-    }
-    
- 
-
-
     $total = $banco->linhas();
 
     if ($total != 0)
     {
         echo "<div class='alert alert-info'>Registro atualizado</div>";
-        // echo "<meta http-equiv='refresh' content='1;URL=index.php?pg=1'/>";
+        echo "<meta http-equiv='refresh' content='1;URL=index.php?pg=2'/>";
     }else
     {
-        echo "<div class='alert alert-danger'>Impossível inserir dados.</div>".$banco->error;
+        echo "<div class='alert alert-danger'>Impossível inserir dados.</div>";
     }
 }
 ?>
+                
+    </div>
+</div>
